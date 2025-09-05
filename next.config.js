4171f129b1e52f24d9557ea41ae1f6b403be7d53
juniorrrrr345 <208288612+juniorrrrr345@users.noreply.github.com>
2025-09-05 22:27:26 +0200

    🔄 DELIS FOOD MARKET - next.config.js adapté

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: []
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-  env: {
-    CLOUDFLARE_ACCOUNT_ID: '7979421604bd07b3bd34d3ed96222512',
-    CLOUDFLARE_API_TOKEN: 'ijkVhaXCw6LSddIMIMxwPL5CDAWznxip5x9I1bNW',
-    CLOUDFLARE_DATABASE_ID: 'b52bc539-a06c-4c32-9569-0ea917199a6b'
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Configuration optimisée pour Vercel
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose']
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src'),
+    }
+    return config
   }
 }
 
